425e05a257e99e6c8a3cb9860f8e02bfbada3c40
iselink <33873947+iselink@users.noreply.github.com>
2021-10-16 17:31:51 +0200

    added cell value field

--- src/ui.c
+++ src/ui.c
@@ -15,6 +15,7 @@ WINDOW *g_window = NULL;
 //all header windows
 WINDOW *g_winHeader = NULL;
 WINDOW *g_winHeaderCellEntry = NULL;
+WINDOW *g_winHeaderDataEntry = NULL;
 
 //all sheet releated windows
 WINDOW *g_winSheet = NULL;
@@ -25,6 +26,16 @@ int g_close = 0;
 //size of the header part
 int g_headerSize = 3;
 
+//what thing from TUI have focus
+int focusElement = UI_FOCUS_SHEET;
+
+//field entry
+char dataEntryText[UI_DATA_FIELD_SIZE];
+
+//////////////////////////////////////////////////
+
+void updateEditField(const char *);
+
 //////////////////////////////////////////////////
 
 /**
@@ -32,6 +43,8 @@ int g_headerSize = 3;
 **/
 //TODO: init error handling
 void uiInitiate() {
+	memset(dataEntryText, 0, sizeof(char) * UI_DATA_FIELD_SIZE);
+
 	g_window = initscr();
 
 	//TODO: more colors!
@@ -40,6 +53,7 @@ void uiInitiate() {
 		init_pair(UI_COLOR_DEFAULT, COLOR_WHITE, COLOR_BLACK);
 		init_pair(UI_COLOR_HEADER, COLOR_WHITE, COLOR_BLUE);
 		init_pair(UI_COLOR_SHEET, COLOR_GREEN, COLOR_BLACK);
+		init_pair(UI_COLOR_SHEET_FOCUS, COLOR_CYAN, COLOR_BLACK);
 	}
 
 	noecho();
@@ -48,6 +62,7 @@ void uiInitiate() {
 
 	g_winHeader = newwin(g_headerSize, COLS, 0, 0);
 	g_winHeaderCellEntry = subwin(g_winHeader, 1, 16, 1, 1);
+	g_winHeaderDataEntry = subwin(g_winHeader, 1, 64, 1, 18);
 	g_winSheet = newwin(LINES - g_headerSize, COLS, g_headerSize, 0);
 }
 
@@ -56,9 +71,11 @@ void uiInitiate() {
 **/
 void uiDispose() {
 	delwin(g_winHeaderCellEntry);
+	delwin(g_winHeaderDataEntry);
 	delwin(g_winHeader);
 	delwin(g_winSheet);
 	g_winHeaderCellEntry = NULL;
+	g_winHeaderDataEntry = NULL;
 	g_winHeader = NULL;
 	g_winSheet = NULL;
 	endwin();
@@ -68,29 +85,84 @@ void uiDispose() {
  * Handle key presses.
 **/
 void uiHandleInput() {
-	switch (getch()) {
+	int key = getch();
+
+	switch (key) {
+		//quit
 		case KEY_F(10):
 		case 'Q':
 		case 'q': {
 			g_close = 1;
 			break;
 		}
+
 		//cursor movement
 		//TODO: add also viewport movement (with scr lock)
+		//TODO: implement cur movement in the fields
 		case KEY_LEFT: {
 			setCursor(getCursorPositionY(), getCursorPositionX() - 1);
+			if (isCursorPositionValid()) {
+				updateEditField(getCellValue());
+			} else {
+				updateEditField(NULL);
+			}
 			break;
 		}
 		case KEY_RIGHT: {
 			setCursor(getCursorPositionY(), getCursorPositionX() + 1);
+			if (isCursorPositionValid()) {
+				updateEditField(getCellValue());
+			} else {
+				updateEditField(NULL);
+			}
 			break;
 		}
 		case KEY_UP: {
 			setCursor(getCursorPositionY() - 1, getCursorPositionX());
+			if (isCursorPositionValid()) {
+				updateEditField(getCellValue());
+			} else {
+				updateEditField(NULL);
+			}
 			break;
 		}
 		case KEY_DOWN: {
 			setCursor(getCursorPositionY() + 1, getCursorPositionX());
+			if (isCursorPositionValid()) {
+				updateEditField(getCellValue());
+			} else {
+				updateEditField(NULL);
+			}
+			break;
+		}
+
+		case KEY_DC:
+		case KEY_BACKSPACE: {
+			if (isSheetAvailable() && isCursorPositionValid()) {
+				setCellValue(NULL);
+			}
+			break;
+		}
+
+		case ' ': {
+			if (focusElement == UI_FOCUS_SHEET) {
+				focusElement = UI_FOCUS_CELL_VALUE;
+			}
+			break;
+		}
+
+		//escape key handling
+		case KEY_F(9): {
+			switch (focusElement) {
+				case UI_FOCUS_CELL_VALUE: {
+					focusElement = UI_FOCUS_SHEET;
+					break;
+				}
+				case UI_FOCUS_SHEET: {
+					focusElement = UI_FOCUS_CELL_VALUE;
+					break;
+				}
+			}
 			break;
 		}
 	}
@@ -103,11 +175,19 @@ void uiRefreshScreen() {
 
 	wbkgd(g_winHeaderCellEntry, '_' | COLOR_PAIR(UI_COLOR_SHEET));
 	wclear(g_winHeaderCellEntry);
+
+	wbkgd(g_winHeaderDataEntry, ' ' | COLOR_PAIR(focusElement == UI_FOCUS_CELL_VALUE ? UI_COLOR_SHEET : UI_COLOR_SHEET_FOCUS));
+	wclear(g_winHeaderDataEntry);
+
 	if (isCursorPositionValid()) {
+		//cell position code
 		char tmp[7];
 		memset(tmp, 0, sizeof(char) * 7);
 		sheetTranslateColToLetter(getCursorPositionX(), 7, tmp);
 		mvwprintw(g_winHeaderCellEntry, 0, 0, "%s%i (%ix%i)", tmp, getCursorPositionY(), getCursorPositionX(), getCursorPositionY());
+
+		//cell's value
+		mvwaddnstr(g_winHeaderDataEntry, 0, 0, dataEntryText, UI_DATA_FIELD_SIZE);
 	} else {
 		mvwprintw(g_winHeaderCellEntry, 0, 0, "-/-");
 	}
@@ -123,7 +203,7 @@ void uiRefreshScreen() {
 			int realRow = y;
 			const char *value = getCellValueAt(x, y);
 			if (value != NULL) {
-				attr_t attr = getCursorPositionX() == x && getCursorPositionY() == y ? A_REVERSE : 0;
+				attr_t attr = (focusElement == UI_FOCUS_SHEET && getCursorPositionX() == x && getCursorPositionY() == y) ? A_REVERSE : 0;
 				wattron(g_winSheet, attr);
 				for (int i = 0; i < 16; i++) {
 					if (value[i] != '\0') {
@@ -142,9 +222,23 @@ void uiRefreshScreen() {
 	refresh();
 	wrefresh(g_winHeader);
 	wrefresh(g_winHeaderCellEntry);
+	wrefresh(g_winHeaderDataEntry);
 	wrefresh(g_winSheet);
 }
 
 int uiShouldClose() {
 	return g_close;
+}
+
+/**
+ * Update that pesky edit field that is in the header.
+ * If null is passed, then field will be reseted.
+**/
+void updateEditField(const char *text) {
+	if (text == NULL) {
+		memset(dataEntryText, 0, sizeof(char) * UI_DATA_FIELD_SIZE);
+	} else {
+		//copy text to dataEntryText with max UI_DATA_FIELD_SIZE chars
+		strncpy(dataEntryText, text, UI_DATA_FIELD_SIZE);
+	}
 }
--- src/ui.h
+++ src/ui.h
@@ -1,9 +1,19 @@
 #ifndef INCLUDED_UI
 #define INCLUDED_UI
 
+//colors!
 #define UI_COLOR_DEFAULT 0
 #define UI_COLOR_HEADER 1
 #define UI_COLOR_SHEET 2
+#define UI_COLOR_SHEET_FOCUS 3
+
+//ui focus elements
+#define UI_FOCUS_NONE 0
+#define UI_FOCUS_SHEET 1
+#define UI_FOCUS_CELL_POSITION 2
+#define UI_FOCUS_CELL_VALUE 3
+
+#define UI_DATA_FIELD_SIZE 256
 
 void uiInitiate();
 void uiDispose();
@@ -11,6 +21,5 @@ void uiDispose();
 void uiHandleInput();
 int uiShouldClose();
 void uiRefreshScreen();
-
 
 #endif	// !INCLUDED_UI
